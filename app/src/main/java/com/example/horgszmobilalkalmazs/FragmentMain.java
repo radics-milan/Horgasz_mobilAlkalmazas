0425d688bf4a5e2a59e113956d3c7ffde6bc023d
Radics Milán <h985007@stud.u-szeged.hu>
2022-12-03 18:10:19 +0100

    Apróbb javítások kinézet és szöveg hibák

--- app/src/main/java/com/example/horgszmobilalkalmazs/FragmentMain.java
+++ app/src/main/java/com/example/horgszmobilalkalmazs/FragmentMain.java
@@ -21,6 +21,8 @@ public class FragmentMain extends Fragment {
     private DatabaseFish databaseFish;
     private boolean showCloseSeasonalFish = true;
 
+    private ArrayList<ClassFish> todayCloseSeasonalFishArrayList;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_main, container, false);
@@ -31,8 +33,27 @@ public class FragmentMain extends Fragment {
         todayCloseSeasonalFishTextView = view.findViewById(R.id.today_s_close_seasonal_fish_textView);
         todayCloseSeasonalFishTextView.setOnClickListener(o -> changeVisibility());
         databaseFish = new DatabaseFish(getActivity());
-        show();
+        databaseFish.fillLocalStore();
+        fillTodayCloseSeasonalFishArrayList();
         return view;
+    }
+
+    private void fillTodayCloseSeasonalFishArrayList() {
+        todayCloseSeasonalFishArrayList = new ArrayList<>();
+        ArrayList<ClassFish> fish = databaseFish.getAllDataFromLocalStore();
+
+        for (ClassFish hal: fish) {
+            if(hal.isCloseSeasonToday()) {
+                todayCloseSeasonalFishArrayList.add(hal);
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        showCloseSeasonalFish = true;
+        show();
     }
 
     private void changeVisibility(){
@@ -41,33 +62,30 @@ public class FragmentMain extends Fragment {
     }
 
     private void show() {
-        int counter = 0;
         todayCloseSeasonalFishLinearLayout.removeAllViews();
         if (showCloseSeasonalFish){
-            ArrayList<ClassFish> fish = databaseFish.getAllDataFromLocalStore();
-
-            for (ClassFish hal: fish) {
-                if(hal.isCloseSeasonToday()){
-                    counter++;
-                    TextView fishListItem = new TextView(getActivity());
-                    fishListItem.setText(hal.getName());
-                    fishListItem.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
-                    fishListItem.setTextColor(getResources().getColor(R.color.black));
-                    fishListItem.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-                    fishListItem.setOnClickListener(view -> onClickFish(hal.getName()));
-                    todayCloseSeasonalFishLinearLayout.addView(fishListItem);
-                }
-            }
-            if (counter == 0) {
+            if (todayCloseSeasonalFishArrayList.size() == 0) {
                 todayCloseSeasonalFishTextView.setText("Ma nincs tilalmi időszakos hal.");
                 todayCloseSeasonalFishTextView.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
             } else {
-                todayCloseSeasonalFishTextView.setText("Mai tilalmi időszakos halak (" + counter + " db)");
+                todayCloseSeasonalFishTextView.setText("Mai tilalmi időszakos halak (" + todayCloseSeasonalFishArrayList.size() + " db)");
                 todayCloseSeasonalFishTextView.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_arrow_drop_up, 0);
+                for (ClassFish fish : todayCloseSeasonalFishArrayList) {
+                    TextView fishListItem = new TextView(getActivity());
+                    fishListItem.setText(fish.getName());
+                    fishListItem.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
+                    fishListItem.setTextColor(getResources().getColor(R.color.black));
+                    fishListItem.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                    fishListItem.setOnClickListener(view -> onClickFish(fish.getName()));
+                    todayCloseSeasonalFishLinearLayout.addView(fishListItem);
+                }
             }
         } else {
-            todayCloseSeasonalFishLinearLayout.removeAllViews();
-            todayCloseSeasonalFishTextView.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_arrow_drop_down, 0);
+            if (todayCloseSeasonalFishArrayList.size() == 0) {
+                todayCloseSeasonalFishTextView.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+            } else {
+                todayCloseSeasonalFishTextView.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_arrow_drop_down, 0);
+            }
         }
     }
 
